94e37a885565d847827b75740f17f0c6042b3061
Mathew <contact@lectro.moe>
2023-01-05 20:26:15 +0300

    add landing

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,14 @@ use dioxus::prelude::*;
 use dioxus_router::*;
 
 pub const PUBLIC_URL: &str = "/";
-pub const LECTRO_GRADIENT: &str = "bg-gradient-to-r from-red-200 via-red-300 to-yellow-200";
+pub const LECTRO_GRADIENT: &str =
+    "inline-block bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 text-transparent bg-clip-text";
+pub const RUST_GRADIENT: &str =
+    "inline-block bg-gradient-to-r from-red-300 via-red-200  to-yellow-100 text-transparent bg-clip-text";
+pub const CPP_GRADIENT: &str =
+    "inline-block bg-gradient-to-r from-sky-300 to-sky-200 text-transparent bg-clip-text";
+pub const TS_GRADIENT: &str =
+    "inline-block bg-gradient-to-r from-violet-300 to-violet-200 text-transparent bg-clip-text";
 
 fn main() {
     dioxus_web::launch(App);
@@ -13,12 +20,12 @@ fn main() {
 fn App(cx: Scope) -> Element {
     cx.render(rsx! {
         Background { }
-        main {
-            class: "roboto-mono flex flex-col w-full md:w-3/4 xl:w-1/2 mx-auto text-stone-200",
+        main { class: "roboto-mono flex flex-col w-max h-screen sm:w-3/4 xl:w-1/2 mx-auto text-xl text-stone-200",
             Router {
                 Route { to: "/", Home {} }
                 Route { to: "/projects", Projects {} }
                 Route { to: "/articles", Articles {} }
+                Route { to: "/contacts", Contacts {} }
             }
         }
     })
@@ -32,23 +39,17 @@ fn TopBar(cx: Scope) -> Element {
         )
     });
     let title = rsx! { h3 { class: title_class.as_str(), "lectro.moe"}};
+
     let router = use_route(cx);
     let url = router.last_segment()?;
-
-    let bar = match url {
+    let links = match url {
         "" => rsx! {
-                div {
-                    class: "flex w-full flex-row place-items-center place-content-around w-full",
-                    Link { to: "/projects", "projects" }
-                    Link { to: "/articles", "articles" }
-                    Link { to: "/contacts", "contacts" }
-                }
+            Link { to: "/projects", "projects" }
+            Link { to: "/articles", "articles" }
+            Link { to: "/contacts", "contacts" }
         },
         _ => rsx! {
-                div {
-                    class: "flex w-full flex-row place-items-center place-content-around w-full",
-                    Link { to: "/", "< back" }
-                }
+            Link { to: "/", "< back" }
         },
     };
 
@@ -56,55 +57,58 @@ fn TopBar(cx: Scope) -> Element {
         div {
             class: "flex flex-col items-center",
             Link { to: "/", title }
-            bar
+            div { class: "flex flex-row place-items-center place-content-around w-full",
+                links
+            }
         }
     })
 }
 
 fn Home(cx: Scope) -> Element {
+    let languages = rsx! {
+        span { class: "flex flex-row",
+            p { ">  " }
+            p { class: RUST_GRADIENT, "Rust" }
+            p { ", C++, TypeScript" }
+        }
+    };
+
     cx.render(rsx! {
-        div {
-            TopBar { }
-            { " hi "}
+        TopBar { }
+        div { class: "flex flex-col items-center place-content-evenly lg:flex-row h-full",
+            div { class: "flex flex-col items-left place-content-around h-24",
+                h3 { " > Software engineer" }
+                languages
+                h3 { " > I like to keep things clean" }
+            }
+            div { class: "flex flex-col text-right place-content-around h-16",
+                h3 { "github: " a { class: "underline", href:"https://github.com/lectromoe", "@lectromoe" } }
+                h3 { " " }
+                h3 { "hr: " a { class: "underline", href:"mailto:jobs@lectro.moe", "jobs@lectro.moe" }  }
+        
+            }
         }
     })
 }
 
 fn Projects(cx: Scope) -> Element {
     cx.render(rsx! {
-        div {
-            TopBar { }
-            { " proj "}
-        }
+        TopBar { }
+        " proj "
     })
 }
 
 fn Articles(cx: Scope) -> Element {
     cx.render(rsx! {
-        div {
-            TopBar { }
-            { " article "}
-        }
+        TopBar { }
+        " article "
     })
 }
 
-fn Avatar(cx: Scope) -> Element {
-    let onclick = |_| println!("click!");
-    let animation = use_state(cx, || "");
-    let class = use_state(cx, || {
-        format!(
-            "flex shrink-0 items-center rounded-full p-1 m-4 w-20 {} {}",
-            LECTRO_GRADIENT, *animation
-        )
-    });
-
+fn Contacts(cx: Scope) -> Element {
     cx.render(rsx! {
-        div {
-            class: class.as_str(),
-            role: "button", class: "", onclick: onclick,
-            img { src: "assets/img/ps2doggy.png", alt: "doggy", class: "rounded-full",
-            }
-        }
+        TopBar { }
+        "contacts"
     })
 }
 
