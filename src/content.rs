109c8e20df798d84221492e7a560e3ed7bfe8c4a
kualta <contact@kualta.dev>
2023-01-27 14:31:09 +0300

    update data schema

--- src/content.rs
+++ src/content.rs
@@ -1,34 +1,19 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Default, Deserialize, Debug, Clone, PartialEq, Eq)]
-pub struct Data {
-    pub projects: Vec<Project>,
-    pub articles: Vec<Article>,
-    pub contacts: Vec<Contact>,
+pub struct DataSchema {
+    pub projects: Vec<Entry>,
+    pub articles: Vec<Entry>,
+    pub contacts: Vec<Entry>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
-pub struct Project {
-    pub name: String,
-    pub date: String,
-    pub stack: String,
-    pub status: String,
-    pub description: String,
-    pub language: String,
-    pub links: Vec<(String, String)>,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
-pub struct Article {
+pub struct Entry {
     pub name: String,
     pub description: String,
-    pub theme: String,
-    pub links: Vec<(String, String)>,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
-pub struct Contact {
-    pub name: String,
-    pub handle: String,
     pub link: String,
+    pub theme: Option<String>,
+    pub date: Option<String>,
+    pub stack: Option<String>,
+    pub status: Option<String>,
 }
--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,13 @@ mod content;
 
 use std::ops::Deref;
 
-use content::{Article, Contact, Data, Project};
+use content::{DataSchema, Entry};
 use dioxus::prelude::*;
 use dioxus_free_icons::icons::bs_icons::{
-    BsCaretDown, BsDiscord, BsEnvelopeFill, BsGithub, BsLink45deg, BsMastodon, BsTelegram,
-    BsTwitter, BsYoutube,
+    BsDiscord, BsEnvelopeFill, BsGithub, BsLink45deg, BsMastodon, BsTelegram, BsTwitter, BsYoutube,
 };
 use dioxus_free_icons::Icon;
-use dioxus_router::*;
 use rand::seq::SliceRandom;
-use rand::thread_rng;
 use serde::de::DeserializeOwned;
 
 pub const PUBLIC_URL: &str = "/";
@@ -26,7 +23,7 @@ fn main() {
 
 fn App(cx: Scope) -> Element {
     let data = use_future(cx, (), |_| async move {
-        match fetch_data::<Data>(DATA_URL).await {
+        match fetch_data::<DataSchema>(DATA_URL).await {
             Ok(data) => data,
             Err(err) => {
                 log::error!("{:?}", err);
@@ -48,19 +45,19 @@ fn App(cx: Scope) -> Element {
     cx.render(rsx! {
         main { class: "mx-auto flex flex-col min-h-screen max-w-2xl text-stone-200",
             Header { contacts: contacts }
-            Projects { projects: projects }
-            Articles { articles: articles }
+            DataList { projects: projects }
+            DataList { projects: articles }
         }
     })
 }
 
 #[inline_props]
-fn Header(cx: Scope, contacts: Vec<Contact>) -> Element {
+fn Header(cx: Scope, contacts: Vec<Entry>) -> Element {
     let mut rng = rand::thread_rng();
     let dict = vec![
-        "simply makes things", 
+        "simply makes things",
         "makes simple things",
-        "makes things simple", 
+        "makes things simple",
     ];
     let text = dict.choose(&mut rng).unwrap().deref();
     cx.render(rsx! {
@@ -82,69 +79,21 @@ where
 }
 
 #[inline_props]
-fn Projects(cx: Scope, projects: Vec<Project>) -> Element {
-    let projects = rsx! {
-        table { class: "relative table-auto",
-            tr { class: "text-left",
-                th { class: "px-4 ", "name" }
-                th { class: "px-4 ",                  "description" }
-                th { class: "px-4 text-center",       "language" }
-                th { class: "px-4 ",                  "stack" }
-                th { class: "px-4 ",                  "links" }
-            }
+fn DataList(cx: Scope, projects: Vec<Entry>) -> Element {
+    cx.render(rsx! {
+        div { class: "relative table-auto",
             projects.iter().map(|project| rsx! {
                 tr { class: "border-neutral-800 border-y-2 text-left",
-                    td { class: "px-4 font-bold",             project.name.as_str() }
-                    td { class: "px-4",                       project.description.as_str() }
-                    td { class: "px-4 text-center",           project.language.as_str() }
-                    td { class: "px-4 py-2",                  project.stack.as_str() }
-                    td { class: "px-4", project.links.iter().map(|link| rsx! {
-                        a { class: "underline pr-2", href: link.1.as_str(), link.0.as_str()}})
+                    a { href: project.link.as_str(),
+                        match &project.theme {
+                            Some(theme) => rsx! { td { class: "px-4 font-bold", theme.as_str() } },
+                            None => rsx!("")
+                        }
+                        td { class: "px-4 font-bold", project.name.as_str() }
+                        td { class: "px-4", project.description.as_str() }
                     }
                 }
             })
-        }
-    };
-
-    cx.render(rsx! {
-        div { class: "h-screen flex roboto-mono text-center flex-col place-items-center justify-center text-xs sm:text-sm md:text-base", id: "content",
-            div { class: "mt-16 mb-10 ",
-                h1 { class: "font-bold text-4xl sm:text-6xl text-neutral-800 my-4", "Projects" }
-                projects
-            }
-        }
-    })
-}
-
-#[inline_props]
-fn Articles(cx: Scope, articles: Vec<Article>) -> Element {
-    let articles = rsx! {
-        table { class: "relative table-auto",
-            tr { class: "text-left",
-                th { class: "px-4", "name" }
-                th { class: "px-4",            "description" }
-                th { class: "px-4 text-center", "theme" }
-                th { class: "px-4",            "links" }
-            }
-            articles.iter().map(|article| rsx! {
-                tr { class: "border-neutral-800 border-y-2 text-left",
-                    td { class: "px-4 font-bold", article.name.as_str() }
-                    td { class: "px-4",                      article.description.as_str() }
-                    td { class: "px-4 py-2 text-center",                  article.theme.as_str() }
-                    td { class: "px-4", article.links.iter().map(|link| rsx! {
-                        a { class: "underline pr-2", href: link.1.as_str(), link.0.as_str()}})
-                    }
-                }
-            })
-        }
-    };
-
-    cx.render(rsx! {
-        div { class: "h-screen flex roboto-mono text-center flex-col place-items-center justify-center text-xs sm:text-sm md:text-base", id: "content",
-            div { class: "mt-16 mb-10",
-                h1 { class: "font-bold text-4xl sm:text-6xl text-neutral-800 my-4", "Articles" }
-                articles
-            }
         }
     })
 }
@@ -164,7 +113,7 @@ fn SocialIcon(name: &str) -> LazyNodes<'_, '_>{
 }
 
 #[inline_props]
-fn Contacts(cx: Scope, contacts: Vec<Contact>) -> Element {
+fn Contacts(cx: Scope, contacts: Vec<Entry>) -> Element {
     let contacts = contacts.iter().map(|contact| {
         let icon = SocialIcon(&contact.name);
 
